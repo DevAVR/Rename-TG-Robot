68a40b817641c4565511c4291ccd55fe2665a86e
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-11-24 15:32:45 +0530

    Initial commit

---
+++ translation.py
@@ -0,0 +1,41 @@
+class Translation(object):
+    START_TEXT = """ Simple Telegram Renaming BOT Edited By This [MAN](https://tx.me/AID_3N)
+              🎈 @DX_BOTZ 🎈
+<b>Please send me any Telegram file and reply /rename New Name.extn</b>
+SEE /help For My Features"""
+
+    RENAME_403_ERR = "There Is Some Error With The File"
+    ABS_TEXT = "What Are You Trying To Do,Mate? You Are Banned!"
+    UPGRADE_TEXT = "CONTACT @AID_3N"
+    DOWNLOAD_START = "Downloading😊"
+    UPLOAD_START = "Uploading✨"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Next File **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set 😂😂😏"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Pooyee🤣🤣"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
+    CUSTOM_CAPTION_UL_FILE = """⊱ ──────  𝗖𝗣𝗥 ────── ⊰
+𝙼𝚕𝚢𝚕𝚖   ➣@CPR_Malayalam
+𝙰𝚕𝚕        ➣@CPRLINKS
+𝚂𝚎𝚛𝚒𝚎𝚜 ➣@CPR_TV_SERIES
+𝙷𝚒𝚗𝚍𝚒   ➣@CPR_HINDI
+𝚃𝚊𝚖𝚒𝚕   ➣@CPR_Tamil
+𝙷𝙴𝚅𝙲      ➣@CPR_HEVC
+𝙸𝚗𝚏𝚘      ➣@CPR_DVDINFO"""
+    HELP_USER = """<b>How To Use Me</b>
+    1. Send me any Telegram File(Some Files Gets Error)
+    2. Reply to that message to /rename NEW NAME.EXTENSION
+    FEATURES
+    ➡Rename Telegram File With Custom Thumbnail.
+    ➡Convert Streamable File(Video) To File.
+    ➡ FOR More Join @DX_BOTZ"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
+    IFLONG_FILE_NAME = """Cheriya Name Aaakadeey"""
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
+    CANCEL_STR = "Process Cancelled"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
