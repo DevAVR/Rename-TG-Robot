7b23f8129742932ba28e8ebb44642df6b236170c
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-11-24 15:32:55 +0530

    Initialize patch-4

---
+++ translation.py
@@ -0,0 +1,34 @@
+class Translation(object):
+    START_TEXT = """ Simple Telegram File Renaming BOT
+              🎈 @DX_BOTZ 🎈
+<b>Please send me any Telegram file and reply /rename If  New Name.extn</b>
+SEE /help For My Features"""
+
+    RENAME_403_ERR = "There Is Some Error With The File"
+    ABS_TEXT = "What Are You Trying To Do,Mate? You Are Banned! Now Get The Fukc Out😃"
+    UPGRADE_TEXT = "CONTACT @AID_3N"
+    DOWNLOAD_START = "Downloading⚡"
+    UPLOAD_START = "Uploading✨"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Next File **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Set 😂😂😏"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Pooyee🤣🤣"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
+    CUSTOM_CAPTION_UL_FILE = """@DX_BOTZ"""
+    HELP_USER = """<b>How To Use Me</b>
+    1. Send me any Telegram File(Some Files Gets Error)
+    2. Reply to that message to /rename NEW NAME.EXTENSION (If Not Working Then Try /change)
+    FEATURES
+    ➡Rename Telegram File With Custom Thumbnail.
+    ➡Convert Streamable File(Video) To File.
+    ➡ FOR More Join @DX_BOTZ"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
+    IFLONG_FILE_NAME = """Cheriya Name Aaakadeey"""
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
+    CANCEL_STR = "Process Cancelled"
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
